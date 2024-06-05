18a44823ab1f4785095c3d97b2f7511e1baef4e4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-06-05 16:36:12 +0300

    Create 0005-log-return-visualization.py

---
+++ 0005-log-return-visualization.py
@@ -0,0 +1,13 @@
+# log return visualization
+
+import matplotlib.pyplot as plt
+
+garanti['log return'].plot(figsize = (15, 8))
+
+plt.title("Garanti Logaritmic Return")
+
+plt.xlabel("Time")
+
+plt.ylabel("Logaritmic Return")
+
+plt.show()
